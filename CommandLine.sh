e7e1cb9bca9b09bd6227722d2868d4f433cafbc5
Matteo Rampolla <37956397+MRampo@users.noreply.github.com>
2022-11-16 21:52:09 +0100

    Added Command Lined

---
+++ CommandLine.sh
@@ -0,0 +1,17 @@
+words=(Italy Spain France England United\ States)
+for n in "${words[@]}";
+do :
+
+    count=$(cut -f 3,4,8 all_tsv.tsv | grep "$n" | wc -l)
+    
+    sum=$(cut -f 3,4,8 all_tsv.tsv | grep "$n" | awk '{sum+=$1 } END{print (sum)}')
+    mean=$(($sum/$count))
+
+
+    total_wish=$(cut -f 3,4,8 all_tsv.tsv | grep "$n" | awk '{sum+=$2 } END{print sum}')
+
+    echo $n 
+    echo total number of places in $n : $count
+    echo Average visitors of $n : $mean
+    echo Who wants to visit $n in total: $total_wish
+done
